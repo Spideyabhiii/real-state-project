adf3a0828d566036091d8c2d04b032429b4ea33b
Spideyabhiii <abhirathod116@gmail.com>
2024-07-08 19:53:11 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,20 @@ import SignInn from './pagess/SignInn';
 import SignUpp from './pagess/SignUpp';
 import Aboutt from './pagess/Aboutt';
 import Profille from './pagess/Profille';
+import Headerr from './component/Headerr';
+
 
 export default function App(){
-   return <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Homme />} />
-    <Route path="/sign-inn" element={<SignInn />} />
-    <Route path="/sign-upp" element={<SignUpp />} />
-    <Route path="/aboutt" element={<Aboutt />} />
-    <Route path="/profille" element={<Profille />} />
-
-
-   </Routes>
+   return ( 
+       <BrowserRouter>
+       <Headerr/>
+      <Routes>   
+         <Route path="/" element={<Homme />} />
+         <Route path="/sign-inn" element={<SignInn />} />
+         <Route path="/sign-upp" element={<SignUpp />} />
+         <Route path="/aboutt" element={<Aboutt />} />
+         <Route path="/profille" element={<Profille />} />
+      </Routes>
    </BrowserRouter>
+   );
 }
