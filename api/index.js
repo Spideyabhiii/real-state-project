2f5627f6c52b9e0932f53b35913a6da142852953
Spideyabhiii <abhirathod116@gmail.com>
2024-07-10 18:23:27 +0530

    connect to databse

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+ import dotenv from 'dotenv';
+ dotenv.config();
+
+mongoose.connect(process.env.MONGO).then (() =>{
+    console.log('connected to database');
+    }).catch (err =>{
+        console.log(err);
+    });
+
+
+
 const app = express();
+
 app.listen(4000, () =>{
     console.log('Server is running on port 4000');
 });
